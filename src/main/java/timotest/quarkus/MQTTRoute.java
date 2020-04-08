a8ac8865ba6fca978fb9041ecbd4ba82cdfdd7ec
Timo Friman <tfriman@redhat.com>
2020-04-08 12:14:15 +0300

    Initial commit

---
+++ src/main/java/timotest/quarkus/MQTTRoute.java
@@ -0,0 +1,34 @@
+package timotest.quarkus;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+import static org.apache.camel.Exchange.*;
+import static org.apache.camel.Exchange.HTTP_METHOD;
+
+public class MQTTRoute extends RouteBuilder {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MQTTRoute.class);
+
+    @Override
+    public void configure() throws Exception {
+        from("paho:json-payload")
+        //from("timer:foo?period=10s")
+                .log("got message: ${body}")
+                .process(in -> {
+                    in.getIn().setHeader(CONTENT_TYPE, "application/vnd.kafka.json.v2+json");
+                    String newBody = createKafkaMessage(in.getIn().getBody(String.class));
+                    in.getIn().setBody(newBody);
+                    in.getIn().setHeader(HTTP_METHOD, "POST");
+                })
+                .to("log:sender?showHeaders=true")
+                .to("https:kafka-bridge-kafka-iot.apps.ocp4.timofriman.com:443/topics/test-topic?ignoreResponseBody=true");
+    }
+
+    private String createKafkaMessage(String body) {
+        return "{\"records\": [  {\"value\":" + body + "} ]}";
+    }
+}
