f50203c11cc41b5f15c2042c7508687a50f01ee9
Timo Friman <tfriman@redhat.com>
2020-04-08 15:45:47 +0300

    Configurations added

--- src/main/java/timotest/quarkus/MQTTRoute.java
+++ src/main/java/timotest/quarkus/MQTTRoute.java
@@ -15,8 +15,7 @@ public class MQTTRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("paho:json-payload")
-        //from("timer:foo?period=10s")
+        from("paho:{{mqtt.config}}")
                 .log("got message: ${body}")
                 .process(in -> {
                     in.getIn().setHeader(CONTENT_TYPE, "application/vnd.kafka.json.v2+json");
@@ -25,7 +24,7 @@ public class MQTTRoute extends RouteBuilder {
                     in.getIn().setHeader(HTTP_METHOD, "POST");
                 })
                 .to("log:sender?showHeaders=true")
-                .to("https:kafka-bridge-kafka-iot.apps.ocp4.timofriman.com:443/topics/test-topic?ignoreResponseBody=true");
+                .to("https:{{kafka.config}}");
     }
 
     private String createKafkaMessage(String body) {
